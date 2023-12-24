c622f30756ca4f88231b20cfa97c3d9e611277f5
SergioRamirezM <sergior.rmosquera@gmail.com>
2023-12-24 13:37:56 -0500

    initial commit

---
+++ app/src/main/java/com/papelmark/horoscapp/ui/palmistry/PalmistryFragment.kt
@@ -0,0 +1,24 @@
+package com.papelmark.horoscapp.ui.palmistry
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.papelmark.horoscapp.R
+import com.papelmark.horoscapp.databinding.FragmentLuckBinding
+import com.papelmark.horoscapp.databinding.FragmentPalmistryBinding
+
+class PalmistryFragment : Fragment() {
+
+    private var _binding: FragmentPalmistryBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentPalmistryBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+}
