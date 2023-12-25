b1b7c7502ccab56691a871098be10d5b627186bd
SergioRamirezM <sergior.rmosquera@gmail.com>
2023-12-24 21:38:03 -0500

    add HoroscopeAdapter and HoroscopeViewModel

---
+++ app/src/main/java/com/papelmark/horoscapp/ui/horoscope/adapter/HoroscopeAdapter.kt
@@ -0,0 +1,28 @@
+package com.papelmark.horoscapp.ui.horoscope.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.papelmark.horoscapp.R
+import com.papelmark.horoscapp.domain.model.HoroscopeInfo
+
+class HoroscopeAdapter(private var horoscopeList: List<HoroscopeInfo> = emptyList()) :
+    RecyclerView.Adapter<HoroscopeViewHolder>() {
+
+    fun updateList(list: List<HoroscopeInfo>){
+        horoscopeList = list
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HoroscopeViewHolder {
+        return HoroscopeViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_horoscope, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: HoroscopeViewHolder, position: Int) {
+        holder.render(horoscopeList[position])
+    }
+
+    override fun getItemCount() = horoscopeList.size
+}
