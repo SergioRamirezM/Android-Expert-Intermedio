22953524544dfa53337c266195a56e39ba6d33bf
SergioRamirezM <sergior.rmosquera@gmail.com>
2023-12-25 14:21:06 -0500

    include image

--- app/src/main/java/com/papelmark/horoscapp/ui/detail/HoroscopeDetailActivity.kt
+++ app/src/main/java/com/papelmark/horoscapp/ui/detail/HoroscopeDetailActivity.kt
@@ -2,8 +2,12 @@ package com.papelmark.horoscapp.ui.detail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.navigation.NavArgs
+import androidx.navigation.navArgs
 import com.papelmark.horoscapp.R
 import com.papelmark.horoscapp.databinding.ActivityHoroscopeDetailBinding
+import com.papelmark.horoscapp.ui.horoscope.HoroscopeViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import dagger.hilt.android.WithFragmentBindings
 
@@ -11,10 +15,14 @@ import dagger.hilt.android.WithFragmentBindings
 class HoroscopeDetailActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHoroscopeDetailBinding
+    private val horoscopeDetailViewModel: HoroscopeDetailViewModel by viewModels()
+
+    private val args:HoroscopeDetailActivityArgs by navArgs()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHoroscopeDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        args.type
     }
 }
--- app/src/main/java/com/papelmark/horoscapp/ui/horoscope/HoroscopeFragment.kt
+++ app/src/main/java/com/papelmark/horoscapp/ui/horoscope/HoroscopeFragment.kt
@@ -1,19 +1,19 @@
 package com.papelmark.horoscapp.ui.horoscope
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.papelmark.horoscapp.databinding.FragmentHoroscopeBinding
+import com.papelmark.horoscapp.domain.model.HoroscopeInfo.*
+import com.papelmark.horoscapp.domain.model.HoroscopeModel
 import com.papelmark.horoscapp.ui.horoscope.adapter.HoroscopeAdapter
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
@@ -40,7 +40,25 @@ class HoroscopeFragment : Fragment() {
 
     private fun initList() {
         horoscopeAdapter =
-            HoroscopeAdapter(onItemSelected = { Toast.makeText(context, getString(it.name), Toast.LENGTH_SHORT).show() })
+            HoroscopeAdapter(onItemSelected = {
+                val type = when (it) {
+                    Aquarius -> HoroscopeModel.Aquarius
+                    Aries -> HoroscopeModel.Aries
+                    Cancer -> HoroscopeModel.Cancer
+                    Capricorn -> HoroscopeModel.Capricorn
+                    Gemini -> HoroscopeModel.Gemini
+                    Leo -> HoroscopeModel.Leo
+                    Libra -> HoroscopeModel.Libra
+                    Pisces -> HoroscopeModel.Pisces
+                    Sagittarius -> HoroscopeModel.Sagittarius
+                    Scorpio -> HoroscopeModel.Scorpio
+                    Taurus -> HoroscopeModel.Taurus
+                    Virgo -> HoroscopeModel.Virgo
+                }
+                findNavController().navigate(
+                    HoroscopeFragmentDirections.actionHoroscopeFragmentToHoroscopeDetailActivity(type)
+                )
+            })
 
         binding.rvHoroscope.apply {
             layoutManager = GridLayoutManager(context, 2)
