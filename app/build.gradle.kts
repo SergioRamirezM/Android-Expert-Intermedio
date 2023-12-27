2ff56ffa62eeabbc71de7245f92b89f479713998
SergioRamirezM <sergior.rmosquera@gmail.com>
2023-12-27 12:38:56 -0500

    permissions management

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,19 +58,28 @@ dependencies {
 
     val navVersion = "2.7.1"
     val daggerVersion = "2.48"
+    val retrofitVersion = "2.9.0"
+    val cameraVersion = "1.2.3"
 
-    //NavComponent
+    // NavComponent
     implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
     implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
 
-    //Dagger Hilt
+    // Dagger Hilt
     implementation("com.google.dagger:hilt-android:$daggerVersion")
     kapt("com.google.dagger:hilt-compiler:$daggerVersion")
 
     // Retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
+    implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
     implementation("com.squareup.okhttp3:logging-interceptor:4.3.1")
+
+    // Camera X
+    implementation ("androidx.camera:camera-core:${cameraVersion}")
+    implementation ("androidx.camera:camera-camera2:${cameraVersion}")
+    implementation ("androidx.camera:camera-lifecycle:${cameraVersion}")
+    implementation ("androidx.camera:camera-view:${cameraVersion}")
+    implementation ("androidx.camera:camera-extensions:${cameraVersion}")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
